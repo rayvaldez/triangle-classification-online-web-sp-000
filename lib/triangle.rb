21229cd2b7be455c7cbbbc4d0fbe006388bbd358
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-10-25 16:30:50 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :s1, :s2, :s3
+
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def kind
+    if @s1 && @s2 && @s3
+      return :equilteral
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "The lengths entered are invalid to create a triangle."
+    end
+  end
+
 end
