05f8013488b27616dcda313e594449e3b1da8871
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-10-25 16:43:12 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,14 +10,16 @@ class Triangle
   end
 
   def kind
-      if s1 == s2 && s2 == s3
+      if s1 <= 0 || s2 <= 0 || s3 <= 0
+        raise TriangleError
+      elsif ((s1+s2) <=s3) || ((s1 +s3) <= s2) || ((s2+s3) <= s1)
+        raise TriangleError
+      elsif s1 == s2 && s2 == s3
         return :equilateral
       elsif s1 == s2 || s1 == s3 || s2 == s3
         return :isosceles
       elsif s1 !=s2 || s1 !=s3 || s2 !=s3
         return :scalene
-      elsif s1 <= 0 || s2 <= 0 || s3 <= 0
-        raise TriangleError
       else
         raise TriangleError
       end
